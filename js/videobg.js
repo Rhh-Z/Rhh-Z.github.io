8d0040cf56e41c61fd2ea358eacd6daf5a82073c
Rhh <1466569866@qq.com>
2022-11-23 15:04:40 +0800

    Site updated: 2022-11-23 15:04:37

--- js/videobg.js
+++ js/videobg.js
@@ -7,29 +7,30 @@ function createVideo() {
   source.type = 'video/mp4'
   video.appendChild(source)
   // 根据不同的url地址播放不同的视频
-  if (location.href === 'http://localhost:4000/') {
+  // 一个开发环境一个生产环境
+  if (location.href === 'http://localhost:4000/' || 'https://www.rhhz.top/') {
     source.src = '/video/bg1.mp4'
-  } else if (location.href === 'http://localhost:4000/music/') {
+  } else if (location.href === 'http://localhost:4000/music/' || 'https://www.rhhz.top/music/') {
     source.src = '/video/somke.mp4'
-  } else if (location.href === 'http://localhost:4000/tags/') {
+  } else if (location.href === 'http://localhost:4000/tags/' || 'https://www.rhhz.top/tags/') {
     source.src = '/video/cloud3.mp4'
-  } else if (location.href === 'http://localhost:4000/tags/') {
+  } else if (location.href === 'http://localhost:4000/tags/' || 'https://www.rhhz.top/tags/') {
     source.src = '/video/cloud4.mp4'
-  } else if (location.href === 'http://localhost:4000/categories/') {
+  } else if (location.href === 'http://localhost:4000/categories/' || 'https://www.rhhz.top/categories/') {
     source.src = '/video/bg2.mp4'
-  } else if (location.href === 'http://localhost:4000/archives/') {
+  } else if (location.href === 'http://localhost:4000/archives/' || 'https://www.rhhz.top/archives/') {
     source.src = '/video/cloud2.mp4'
-  } else if (location.href === 'http://localhost:4000/movies/') {
+  } else if (location.href === 'http://localhost:4000/movies/' || 'https://www.rhhz.top/movies/') {
     source.src = '/video/bg6.mp4'
-  } else if (location.href === 'http://localhost:4000/link/') {
+  } else if (location.href === 'http://localhost:4000/link/' || 'https://www.rhhz.top/link/') {
     source.src = '/video/bg4.mp4'
-  } else if (location.href === 'http://localhost:4000/comments/') {
+  } else if (location.href === 'http://localhost:4000/comments/' || 'https://www.rhhz.top/comments/') {
     source.src = '/video/bg5.mp4'
-  } else if (location.href === 'http://localhost:4000/images/') {
+  } else if (location.href === 'http://localhost:4000/images/' || 'https://www.rhhz.top/images/') {
     source.src = '/video/bg7.mp4'
-  } else if (location.href === 'http://localhost:4000/about/') {
+  } else if (location.href === 'http://localhost:4000/about/' || 'https://www.rhhz.top/about/') {
     source.src = '/video/bg8.mp4'
-  } else if (location.href === 'http://localhost:4000/bangumis/') {
+  } else if (location.href === 'http://localhost:4000/bangumis/' || 'https://www.rhhz.top/bangumis/') {
     source.src = 'https://txmov2.a.yximgs.com/upic/2022/11/23/00/BMjAyMjExMjMwMDI1MDJfMjM0NDkwOTU0OF84OTM1MzM3OTY3MV8wXzM=_b_B51b9c96b487f539f48309d2740586a15.mp4', video.muted = false, video.loop = false
     // 去掉标题
     document.getElementById('site-title').innerHTML = ''

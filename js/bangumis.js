75102b8661e6c02ce049e19bb92f0818139300da
Rhh <1466569866@qq.com>
2023-01-20 16:56:03 +0800

    Site updated: 2023-01-20 16:55:45

---
+++ js/bangumis.js
@@ -0,0 +1,9 @@
+// window.addEventListener('load', () => {
+//   let bimg = document.getElementsByClassName('bangumi-picture')
+//   bimgArray = Array.from(bimg)
+//   let src = null
+//   bimgArray.forEach(item => {
+//     src = item.childNodes[0].childNodes[0].getAttribute('data-lazy-src')
+//     item.childNodes[0].childNodes[0].setAttribute("src", src)
+//   });
+// })

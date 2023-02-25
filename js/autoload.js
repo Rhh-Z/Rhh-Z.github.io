31561741471c741b176cd2c8b5b1f689be5a1767
Rhh <1466569866@qq.com>
2023-02-25 23:37:17 +0800

    Site updated: 2023-02-25 23:37:10

--- js/autoload.js
+++ js/autoload.js
@@ -1,1 +1,91 @@
-function showLive(){"use strict";if(!CSS.supports("clip-path","circle(120px at center)"))return void(document.getElementById("stage").innerHTML='<img src="../assets/screenshot-1.png">');const e="https://live2d.fghrsh.net/api";let t=0,o=!1,n=localStorage.getItem("modelId"),l=localStorage.getItem("modelTexturesId");function d(o,n){localStorage.setItem("modelId",o),void 0===n&&(n=0),localStorage.setItem("modelTexturesId",n),loadlive2d("live2d",`${e}/get/?id=${o}-${n}`,null),console.log("live2d",`模型 ${o}-${n} 加载完成`),setTimeout((()=>{s("80%"),t=2}),2e3)}function c(){const n=localStorage.getItem("modelId"),l=localStorage.getItem("modelTexturesId");fetch(`${e}/rand_textures/?id=${n}-${l}`).then((e=>e.json())).then((e=>{d(n,e.textures.id),setTimeout((()=>{t=2,s("80%"),o=!1}),1e3)}))}function i(){const n=localStorage.getItem("modelId");fetch(`${e}/switch/?id=${n}`).then((e=>e.json())).then((e=>{d(e.model.id),setTimeout((()=>{t=2,s("80%"),o=!1}),1e3)}))}function s(e){document.getElementById("cover").style.bottom=e}null===n&&(n=6,l=5),d(n,l),document.getElementById("info").addEventListener("click",(()=>{fetch("https://v1.hitokoto.cn").then((e=>e.json())).then((e=>{document.getElementById("yiyan-content").innerText=e.hitokoto+"------"+e.from}))})),document.getElementById("info").click(),document.getElementById("skin").addEventListener("click",(()=>{o||(t=0,s("10%"),o=!0,setTimeout(i,1e3))})),document.getElementById("model").addEventListener("click",(()=>{o||(t=0,s("10%"),o=!0,setTimeout(c,1e3))}))}"/about/"===location.pathname&&showLive();
+function showLive() {
+  "use strict";
+
+  if (!CSS.supports("clip-path", "circle(120px at center)")) {
+    document.getElementById("stage").innerHTML = '<img src="../assets/screenshot-1.png">';
+    return;
+  }
+
+  const apiPath = "https://live2d.fghrsh.net/api";
+  let state = 0, loading = false,
+    modelId = localStorage.getItem("modelId"),
+    modelTexturesId = localStorage.getItem("modelTexturesId");
+  if (modelId === null) {
+    modelId = 6;
+    modelTexturesId = 5;
+  }
+  loadModel(modelId, modelTexturesId);
+
+  function loadModel(modelId, modelTexturesId) {
+    localStorage.setItem("modelId", modelId);
+    if (modelTexturesId === undefined) modelTexturesId = 0;
+    localStorage.setItem("modelTexturesId", modelTexturesId);
+    loadlive2d("live2d", `${apiPath}/get/?id=${modelId}-${modelTexturesId}`, null);
+    console.log("live2d", `模型 ${modelId}-${modelTexturesId} 加载完成`);
+    setTimeout(() => {
+      coverPosition("80%");
+      state = 2;
+    }, 2000);
+  }
+
+  function loadRandModel() {
+    const modelId = localStorage.getItem("modelId"),
+      modelTexturesId = localStorage.getItem("modelTexturesId");
+    fetch(`${apiPath}/rand_textures/?id=${modelId}-${modelTexturesId}`)
+      .then(response => response.json())
+      .then(result => {
+        loadModel(modelId, result.textures.id);
+        setTimeout(() => {
+          state = 2;
+          coverPosition("80%");
+          loading = false;
+        }, 1000);
+      });
+  }
+
+  function loadOtherModel() {
+    const modelId = localStorage.getItem("modelId");
+    fetch(`${apiPath}/switch/?id=${modelId}`)
+      .then(response => response.json())
+      .then(result => {
+        loadModel(result.model.id);
+        setTimeout(() => {
+          state = 2;
+          coverPosition("80%");
+          loading = false;
+        }, 1000);
+      });
+  }
+
+  function coverPosition(pos) {
+    document.getElementById("cover").style.bottom = pos;
+  }
+
+  document.getElementById("info").addEventListener("click", () => {
+    fetch("https://v1.hitokoto.cn")
+      .then(response => response.json())
+      .then(result => {
+        document.getElementById("yiyan-content").innerText = result.hitokoto + '------' + result.from
+      });
+  })
+  document.getElementById("info").click()
+
+  document.getElementById("skin").addEventListener("click", () => {
+    if (loading) return;
+    state = 0;
+    coverPosition("10%");
+    loading = true;
+    setTimeout(loadOtherModel, 1000);
+  });
+
+  document.getElementById("model").addEventListener("click", () => {
+    if (loading) return;
+    state = 0;
+    coverPosition("10%");
+    loading = true;
+    setTimeout(loadRandModel, 1000);
+  });
+}
+if (location.pathname === '/about/') {
+  showLive()
+}
